4895a33770e218e1945dd7a0a369ee6f8e66e819
Md. Likhon Mia <likhon15-3916@diu.edu.bd>
2024-09-07 10:43:08 -0700

    added

--- two.ts
+++ two.ts
@@ -4,9 +4,8 @@ import * as crypto from 'crypto';
 
 function computeSHA3_256(filePath: string): string {
     const fileBuffer: Buffer = fs.readFileSync(filePath);
-    const hash = crypto.createHash('sha3-256');
-    hash.update(fileBuffer);
-    return hash.digest('hex');
+    const hash = crypto.createHash('sha3-256').update(fileBuffer).digest('hex');
+    return hash
 }
 
 
@@ -24,12 +23,17 @@ let hashes: string[] = files.map((file: string): string => {
     return computeSHA3_256(filePath);
 });
 
+
 hashes.sort();
 
+
+
 const concatenatedHashes: string = hashes.join('');
+
 
 const email: string = 'likhon15-3916@diu.edu.bd'.toLowerCase();
 const finalString: string = concatenatedHashes + email;
 const finalHash: string = crypto.createHash('sha3-256').update(finalString).digest('hex');
 
-console.log(finalHash);
+console.log(finalHash)
+
